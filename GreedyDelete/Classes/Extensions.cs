be6f89dffe4f483706478562e5c29aa21f33c576
Masroor Aijaz <masroor.aijaz08@gmail.com>
2021-12-02 02:02:54 +0500

    Extension stops working on different files fixed.

---
+++ GreedyDelete/Classes/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+
+namespace GreedyDelete.Classes
+{
+    public static class Extensions
+    {
+        public static void HandleTextChange(this WpfTextViewHandler wpfTextViewHandler)
+        {
+            if (wpfTextViewHandler == null || wpfTextViewHandler.IsHandlingChange)
+                return;
+
+            ITextViewLine currentLine = wpfTextViewHandler.TextView.Caret.ContainingTextViewLine;
+            bool isCurrentLineEmpty = string.IsNullOrWhiteSpace(currentLine.Extent.GetText());
+            int currentLineIndex = wpfTextViewHandler.TextView.TextViewLines.IndexOf(wpfTextViewHandler.TextView.Caret.ContainingTextViewLine);
+
+            if (wpfTextViewHandler.FoundEmptyLineInPreviousChange)
+            {
+                if (wpfTextViewHandler.EmptyLineIndexInPreviousChange != currentLineIndex || !isCurrentLineEmpty)
+                {
+                    wpfTextViewHandler.FoundEmptyLineInPreviousChange = false;
+                    wpfTextViewHandler.EmptyLineIndexInPreviousChange = -1;
+                    return;
+                }
+            }
+
+            if (isCurrentLineEmpty)
+            {
+                if (wpfTextViewHandler.EmptyLineIndexInPreviousChange != currentLineIndex || !wpfTextViewHandler.FoundEmptyLineInPreviousChange)
+                {
+                    wpfTextViewHandler.EmptyLineIndexInPreviousChange = currentLineIndex;
+                    wpfTextViewHandler.FoundEmptyLineInPreviousChange = true;
+                    return;
+                }
+
+                wpfTextViewHandler.IsHandlingChange = true;
+
+                RemoveLine(currentLine, wpfTextViewHandler.TextView);
+
+                ITextViewLine lineToMoveCursorTo = wpfTextViewHandler.TextView.TextViewLines[wpfTextViewHandler.TextView.TextViewLines.IndexOf(wpfTextViewHandler.TextView.Caret.ContainingTextViewLine) - 1];
+                while (string.IsNullOrWhiteSpace(lineToMoveCursorTo.Extent.GetText()))
+                {
+                    ITextViewLine tempCopy = lineToMoveCursorTo;
+
+                    lineToMoveCursorTo = wpfTextViewHandler.TextView.TextViewLines[wpfTextViewHandler.TextView.TextViewLines.IndexOf(tempCopy) - 1];
+
+                    RemoveLine(tempCopy, wpfTextViewHandler.TextView);
+                }
+
+                wpfTextViewHandler.TextView.Caret.MoveTo(lineToMoveCursorTo, lineToMoveCursorTo.Right);
+
+                wpfTextViewHandler.FoundEmptyLineInPreviousChange = false;
+                wpfTextViewHandler.EmptyLineIndexInPreviousChange = -1;
+
+                wpfTextViewHandler.IsHandlingChange = false;
+            }
+        }
+
+        private static void RemoveLine(ITextViewLine lineToRemove, IWpfTextView textView)
+        {
+            if (textView == null || lineToRemove == null)
+                return;
+
+            ITextEdit textEdit = textView.TextBuffer.CreateEdit();
+            textEdit.Delete(lineToRemove.Start, lineToRemove.LengthIncludingLineBreak);
+            textEdit.Apply();
+        }
+    }
+}
--- GreedyDelete/VSTextViewListener.cs
+++ GreedyDelete/VSTextViewListener.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using GreedyDelete.Classes;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
 
@@ -8,19 +11,58 @@ namespace GreedyDelete
     [ContentType("code")]
     [Export(typeof(IWpfTextViewCreationListener))]
     [TextViewRole(PredefinedTextViewRoles.Editable)]
-    internal sealed class VSTextViewListener : IWpfTextViewCreationListener
+    internal sealed class VSTextViewCreationListener : IWpfTextViewCreationListener
     {
-        public EmptyLineRemover _emptyLineRemover = null;
+        private List<WpfTextViewHandler> _wpfTextViewHandlers = new List<WpfTextViewHandler>();
 
         public void TextViewCreated(IWpfTextView textView)
         {
-            _emptyLineRemover = new EmptyLineRemover(textView);
-            textView.TextBuffer.PostChanged += new EventHandler(TextBuffer_PostChanged);
+            if (WrapperAlreadyExistsForTextView(textView))
+                return;
 
+            textView.TextBuffer.PostChanged += TextBuffer_PostChanged;
+            textView.Closed += TextView_Closed;
+
+            _wpfTextViewHandlers.Add(new WpfTextViewHandler()
+            {
+                TextView = textView
+            });
         }
+
         private void TextBuffer_PostChanged(object sender, EventArgs e)
         {
-            _emptyLineRemover.HandleTextChange();
+            WpfTextViewHandler handler = GetHandlerForTextBuffer(sender as ITextBuffer);
+
+            if (handler != null)
+                handler.HandleTextChange();
+        }
+
+        private bool WrapperAlreadyExistsForTextView(IWpfTextView textView)
+        {
+            return _wpfTextViewHandlers.FindIndex(x => x.TextView == textView) >= 0;
+        }
+
+        private WpfTextViewHandler GetHandlerForTextView(IWpfTextView textView)
+        {
+            return _wpfTextViewHandlers.Find(x => x.TextView == textView);
+        }
+
+        private WpfTextViewHandler GetHandlerForTextBuffer(ITextBuffer textBuffer)
+        {
+            return _wpfTextViewHandlers.Find(x => x.TextView.TextBuffer == textBuffer);
+        }
+
+        private void TextView_Closed(object sender, EventArgs e)
+        {
+            WpfTextViewHandler handler = GetHandlerForTextView(sender as IWpfTextView);
+
+            if (handler != null)
+            {
+                handler.TextView.TextBuffer.PostChanged -= TextBuffer_PostChanged;
+                handler.TextView.Closed -= TextView_Closed;
+
+                _wpfTextViewHandlers.Remove(handler);
+            }
         }
     }
 }
